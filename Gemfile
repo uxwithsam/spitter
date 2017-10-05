a55d6a1f85370d2c52347d55ecd0d78a985caaf8
SAMANTHA VANHORN <vh.samantha@gmail.com>
2017-10-05 19:55:50 -0400

    Installed Devise gem

--- Gemfile
+++ Gemfile
@@ -5,6 +5,10 @@ git_source(:github) do |repo_name|
   "https://github.com/#{repo_name}.git"
 end
 
+gem 'bootstrap', '~> 4.0.0.beta'
+gem 'jquery-rails'
+gem 'devise'
+
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.4'
@@ -13,8 +17,6 @@ gem 'sqlite3'
 # Use Puma as the app server
 gem 'puma', '~> 3.7'
 # Use SCSS for stylesheets
-gem 'bootstrap', '~> 4.0.0.beta'
-gem 'jquery-rails'
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected #this is protected, anything below this we can't call
+
+  def configure_permitted_parameters #this is a method inside the application controller class
+    #so we can't call it, but we need these sanitizers to be executed. So we trick it. 
+    #So we tell it, BEFORE ANY ACTIONS HAPPEN, run this method first, the before_action above.
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :name, :bio, :location])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :name, :bio, :location])
+  end
 end
